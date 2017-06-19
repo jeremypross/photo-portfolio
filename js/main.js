69c921323dcd6d9b0c2e9c393c67f43dcfabd1bc
Jeremy Ross <jeremypross@gmail.com>
2017-06-19 19:15:44 -0400

    footer toggles display when you open menu

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function(){
   const openNav = () => {
       document.getElementById("mySidenav").style.width = "250px";
       document.getElementById("main").style.marginLeft = "250px";
-      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-      document.getElementById("menu-button").display = "none";
+      // document.body.style.backgroundColor = "rgba(0,0,0,0.25)";
+      document.getElementById("footer").style.display = "none";
   }
 
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
@@ -14,11 +14,10 @@ document.addEventListener('DOMContentLoaded', function(){
       document.getElementById("mySidenav").style.width = "0";
       document.getElementById("main").style.marginLeft = "0";
       document.body.style.backgroundColor = "white";
-      document.getElementById("menu-button").display = "show";
-
+      document.getElementById("footer").style.display = "show";
   }
 
-  document.getElementById('menu-button').addEventListener('click', function(event){
+  document.getElementById('nav-button').addEventListener('click', function(event){
     openNav();
   });
 
