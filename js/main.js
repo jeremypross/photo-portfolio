cce67fccbdf4dc0a5b1245ee119c63d69c39a9f8
Jeremy Ross <jeremypross@gmail.com>
2017-06-19 17:13:03 -0400

    toggleable nav working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-$(document).ready(() => {
+document.addEventListener('DOMContentLoaded', function(){
   console.log('js is linked!');
 
-  // create toggle functionality for nav menu
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+  function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
 
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+      document.body.style.backgroundColor = "white";
+  }
+
+  document.getElementById('menu-button').addEventListener('click', function(event){
+    openNav();
+  });
+
+  document.getElementById('close-btn').addEventListener('click', function(event){
+    closeNav();
+  });
 
 
 });
